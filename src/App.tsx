ef6c14357d8d2ba04a11864a4569677ee1b856f2
Kyle Aquino <kpaquino2@up.edu.ph>
2025-09-29 23:48:35 +0800

    feat: Coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-screen flex bg-black text-white">
+      <div className="flex flex-col justify-center ml-64">
+        <p className="text-xl">Kyle Aquino</p>
+        <p className="text-zinc-600 mb-8">Software Developer</p>
+        <p>Building something cool here.</p>
+        <p>Coming soon.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
