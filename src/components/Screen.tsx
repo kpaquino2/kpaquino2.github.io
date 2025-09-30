d12bcf8e579b4103c20e3449d5e41d37c80fb924
Kyle Aquino <kpaquino2@up.edu.ph>
2025-09-30 23:15:21 +0800

    feat: Initial terminal window

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,9 +1,11 @@
 import Panel from "./Panel";
+import Terminal from "./Terminal";
 
 const Screen = () => {
   return (
     <div className="h-screen w-screen flex flex-col bg-white dark:bg-black">
       <Panel />
+      <Terminal />
     </div>
   );
 };
---
+++ src/components/Terminal.tsx
@@ -0,0 +1,15 @@
+const Terminal = () => {
+  return (
+    <div
+      className="flex flex-col border-1 text-black dark:text-white border-black dark:border-white rounded-lg"
+      style={{
+        width: 300,
+        height: 300,
+      }}
+    >
+      <div className="h-6 border-b"></div>
+    </div>
+  );
+};
+
+export default Terminal;
