f354d6a3b4ab6f6d9e139be41e156440d30ebed7
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-05 00:14:45 +0800

    feat: Opening windows

--- src/components/DesktopItem.tsx
+++ src/components/DesktopItem.tsx
@@ -1,13 +1,17 @@
-const DesktopItem = ({
-  icon,
-  label,
-}: {
-  icon: React.ReactNode;
+import type { ReactNode } from "react";
+
+interface DesktopItemProps {
+  id: string;
+  icon: ReactNode;
   label: string;
-}) => (
+  openWindow: (id: string) => void;
+}
+
+const DesktopItem = ({ id, icon, label, openWindow }: DesktopItemProps) => (
   <button
     aria-label={label}
     className="flex w-18 flex-col items-center p-1 border-1 border-dashed border-transparent focus:border-black dark:focus:border-white focus:ring-0 focus:outline-none"
+    onDoubleClick={() => openWindow(id)}
   >
     {icon}
     <p className="text-xs text-center">{label}</p>
--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,14 +2,21 @@ import DesktopItem from "./DesktopItem";
 import Panel from "./Panel";
 import { FileTextIcon, TerminalWindowIcon } from "@phosphor-icons/react";
 import Terminal from "./windows/Terminal";
-import { useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import { motion } from "motion/react";
+
+const WindowStatus = {
+  OPEN: "open",
+  MINI: "minimized",
+  CLOSED: "closed",
+};
 
 const Screen = () => {
   const constraintsRef = useRef<HTMLDivElement>(null);
 
   const desktopItems = [
     {
+      id: "terminal",
       icon: (
         <TerminalWindowIcon
           size={40}
@@ -20,6 +27,7 @@ const Screen = () => {
       label: "Portfolio Terminal",
     },
     {
+      id: "resume",
       icon: (
         <FileTextIcon
           size={40}
@@ -31,6 +39,20 @@ const Screen = () => {
     },
   ];
 
+  const [windows, setWindows] = useState([
+    {
+      id: "terminal",
+      window: <Terminal constraintsRef={constraintsRef} />,
+      status: WindowStatus.CLOSED,
+    },
+  ]);
+
+  const openWindow = (id: string) => {
+    setWindows((prev) =>
+      prev.map((w) => (w.id !== id ? w : { ...w, status: WindowStatus.OPEN }))
+    );
+  };
+
   return (
     <div className="h-screen w-screen">
       <div className="h-full flex flex-col gap-2 p-4">
@@ -40,10 +62,15 @@ const Screen = () => {
           className="relative flex-1 grid auto-cols-[4.5rem] auto-rows-min gap-2 "
         >
           {desktopItems.map((item) => (
-            <DesktopItem key={item.label} {...item} />
+            <DesktopItem key={item.label} {...item} openWindow={openWindow} />
           ))}
-          <Terminal constraintsRef={constraintsRef} />
+          {windows
+            .filter((w) => w.status === WindowStatus.OPEN)
+            .map((w) => (
+              <Fragment key={w.id}>{w.window}</Fragment>
+            ))}
         </motion.div>
+        {}
       </div>
     </div>
   );
