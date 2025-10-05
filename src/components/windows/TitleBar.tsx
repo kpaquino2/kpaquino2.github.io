b45ebaa1100f6381f1e8f3c1261ab40977ad5a51
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-05 22:04:22 +0800

    chore: Remove maximize icon

--- src/components/windows/TitleBar.tsx
+++ src/components/windows/TitleBar.tsx
@@ -1,4 +1,4 @@
-import { MinusIcon, SquareIcon, XIcon } from "@phosphor-icons/react";
+import { MinusIcon, XIcon } from "@phosphor-icons/react";
 import type { DragControls } from "motion/react";
 import { useDesktop } from "../../lib/hooks/useDesktop";
 
@@ -22,9 +22,6 @@ const TitleBar = ({ id, title, controls }: TitleBarProps) => {
           className="border-l w-8 grid place-items-center"
         >
           <MinusIcon />
-        </button>
-        <button className="border-l w-8 grid place-items-center">
-          <SquareIcon />
         </button>
         <button
           onClick={() => closeApp(id)}
