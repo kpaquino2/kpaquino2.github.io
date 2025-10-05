d30e6c7e5d4332f8c3de768d9ba876d8e9e0d66d
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-05 20:46:17 +0800

    chore: Panel adjustments

--- src/components/panel/Panel.tsx
+++ src/components/panel/Panel.tsx
@@ -8,19 +8,22 @@ const Panel = () => {
   return (
     <div className="flex items-center justify-between h-8  border-1">
       <OpenApps />
-      <DateTime />
-      <div className="flex gap-2">
-        {themes.map((t) => (
-          <button
-            className={
-              "cursor-pointer " +
-              (theme !== t.toLocaleLowerCase() && "opacity-50")
-            }
-            onClick={() => setTheme(t.toLocaleLowerCase())}
-          >
-            {t}
-          </button>
-        ))}
+      <div className="flex mr-2 gap-2">
+        <div className="flex gap-2">
+          {themes.map((t) => (
+            <button
+              key={t}
+              className={
+                "cursor-pointer " +
+                (theme !== t.toLocaleLowerCase() && "opacity-50")
+              }
+              onClick={() => setTheme(t.toLocaleLowerCase())}
+            >
+              {t}
+            </button>
+          ))}
+        </div>
+        <DateTime />
       </div>
     </div>
   );
